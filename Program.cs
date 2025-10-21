9953126df437e168e93ecc924b22fa3287895dac
belladonnagiacomo-ai <belladonna.giacomo@franchettisalviani.net>
2025-10-21 10:34:33 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿namespace numeroPerfetto
+{
+    internal class Program
+    {
+        static bool numeroP(int n)
+        {
+            int somma = 0;
+            for (int i = 1; n > i; i++)
+            {
+                if (n%i == 0)
+                {
+                    somma = i + somma;
+                }
+
+            }
+            if (somma == n)
+            {
+                Console.WriteLine("il numero e perfetto");
+                return true;
+            }
+            
+            
+            Console.WriteLine("il numero non e perfetto");
+            return false;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("dammi un numero da analizzare");
+            int n = Convert.ToInt32(Console.ReadLine());
+            numeroP(n);
+        }
+    }
+}
